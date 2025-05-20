2b70ed455a3b5281ddbefbbd3dc2221faa005df8
rinya7 <v.dmitriev0712@gmail.com>
2025-05-20 15:46:26 +0200

    fix: remove @tailwindcss/postcss to avoid lightningcss error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizeCss: false
+  },
+  compiler: {
+    cssMinifier: 'parcel'
+  }
 };
 
 export default nextConfig;
