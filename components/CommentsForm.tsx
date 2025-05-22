ba386d70f24dd71898b8a98e32e4e627ec2c97fb
rinya7 <v.dmitriev0712@gmail.com>
2025-05-22 16:49:05 +0200

    made add name to comment to login users

--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -1,42 +1,67 @@
-// components/CommentsForm.tsx
 "use client";
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CommentInput, CommentSchema } from "@/lib/zodSchemas";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@/hooks/useAuth";
+import { useEffect } from "react";
 
 export default function CommentsForm({ postId }: { postId: string }) {
   const router = useRouter();
+  const { user } = useAuth();
   const {
     register,
     handleSubmit,
     reset,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<CommentInput>({ resolver: zodResolver(CommentSchema) });
 
+  useEffect(() => {
+    if (user && user.displayName) {
+      setValue("author", user.displayName);
+    }
+  }, [user, setValue]);
+
   const onSubmit = async (data: CommentInput) => {
+    if (user && user.displayName) {
+      data.author = user.displayName;
+    }
     await fetch(`/api/posts/${postId}/comments`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     });
     reset();
-    router.refresh(); // we update the server part (CommentsList)
+    router.refresh();
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-      <div>
-        <input
-          {...register("author")}
-          placeholder="Ваше ім'я"
-          className="w-full p-2 border rounded-lg bg-white dark:bg-gray-800 focus:ring-2 focus:ring-blue-400"
-        />
-        {errors.author && (
-          <p className="text-red-600">{errors.author.message}</p>
-        )}
-      </div>
+      {!user && (
+        <div>
+          <input
+            {...register("author")}
+            placeholder="Ваше ім'я"
+            className="w-full p-2 border rounded-lg bg-white dark:bg-gray-800 focus:ring-2 focus:ring-blue-400"
+          />
+          {errors.author && (
+            <p className="text-red-600">{errors.author.message}</p>
+          )}
+        </div>
+      )}
+
+      {user && (
+        <div>
+          <input
+            {...register("author")}
+            readOnly
+            className="w-full p-2 border rounded-lg bg-gray-100 dark:bg-gray-700 cursor-not-allowed"
+          />
+        </div>
+      )}
+
       <div>
         <textarea
           {...register("text")}
@@ -46,6 +71,7 @@ export default function CommentsForm({ postId }: { postId: string }) {
         />
         {errors.text && <p className="text-red-600">{errors.text.message}</p>}
       </div>
+
       <button
         type="submit"
         disabled={isSubmitting}
